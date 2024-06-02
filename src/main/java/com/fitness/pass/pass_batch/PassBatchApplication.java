a7010862f416a323e0209537ccba82e6520d78b4
Kyle Kim <dkim577@wisc.edu>
2024-06-03 07:51:13 +0900

    first commit

---
+++ src/main/java/com/fitness/pass/pass_batch/PassBatchApplication.java
@@ -0,0 +1,45 @@
+package com.fitness.pass.pass_batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class PassBatchApplication {
+
+	private final JobBuilderFactory jobBuilderFactory;
+	private final StepBuilderFactory stepBuilderFactory;
+
+	public PassBatchApplication(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+		this.jobBuilderFactory = jobBuilderFactory;
+		this.stepBuilderFactory = stepBuilderFactory;
+	}
+
+	@Bean
+	public Step passStep() {
+		return this.stepBuilderFactory.get("passStep")
+				.tasklet((contribution, chunkContext) -> {
+					System.out.println("Pass!");
+					return RepeatStatus.FINISHED;
+				}).build();
+	}
+	@Bean
+	public Job passJob() {
+		return this.jobBuilderFactory.get("passJob")
+				.start(passStep())
+				.build();
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitness/pass/pass_batch/PassBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.pass.pass_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
