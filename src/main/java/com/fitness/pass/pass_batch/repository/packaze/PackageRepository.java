50c0e2a4ec92db57c7761b1c2bce416f1f11f856
Kyle Kim <dkim577@wisc.edu>
2024-06-09 15:44:16 +0900

    Add CRUD Test for packageEntity

--- src/main/java/com/fitness/pass/pass_batch/repository/packaze/PackageRepository.java
+++ src/main/java/com/fitness/pass/pass_batch/repository/packaze/PackageRepository.java
@@ -1,9 +1,24 @@
 package com.fitness.pass.pass_batch.repository.packaze;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface PackageRepository extends JpaRepository<PackageEntity,Integer> {
-
+    List<PackageEntity> findByCreatedAtAfter(LocalDateTime dateTime, Pageable pageable);
+    @Transactional
+    @Modifying // update, delete 쿼리를 실행하기 위한 어노테이션
+    @Query("update PackageEntity p " +
+            "set p.count = :count," +
+            "    p.period = :period " +
+            "where p.packageSeq = :packageSeq")
+    int updateCountAndPeriod(Integer packageSeq, Integer count, Integer period);
 }
--- src/test/java/com/fitness/pass/pass_batch/repository/PackageRepositoryTest.java
+++ src/test/java/com/fitness/pass/pass_batch/repository/PackageRepositoryTest.java
@@ -7,7 +7,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
@@ -27,7 +34,64 @@ public class PackageRepositoryTest {
         packageRepository.save(packageEntity);
 
         // then
-        Assertions.assertNotNull(packageEntity.getPackageSeq());
+        assertNotNull(packageEntity.getPackageSeq());
+    }
+
+    @Test
+    public void test_findByCreatedAtAfter(){
+        //given
+        LocalDateTime dateTime = LocalDateTime.now().minusDays(1);
+
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("학생전용 3개월");
+        packageEntity.setPeriod(90);
+        packageRepository.save(packageEntity);
+
+        PackageEntity packageEntity1 = new PackageEntity();
+        packageEntity1.setPackageName("학생전용 6개월");
+        packageEntity1.setPeriod(180);
+        packageRepository.save(packageEntity1);
+
+        // when
+        final List<PackageEntity> packageEntities = packageRepository.findByCreatedAtAfter(dateTime, PageRequest.of(0, 1, Sort.by("packageSeq").descending()));
+
+        //then
+        assertEquals(1, packageEntities.size());
+        assertEquals(packageEntity1.getPackageSeq(), packageEntities.get(0).getPackageSeq());
+
+    }
+
+    @Test
+    public void test_updateCountAndPeriod(){
+        //given
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("바디프로필 3개월");
+        packageEntity.setPeriod(90);
+        packageRepository.save(packageEntity);
+
+        //when
+        int updatedCount = packageRepository.updateCountAndPeriod( packageEntity.getPackageSeq(),10, 180);
+
+        //then
+        final PackageEntity findPackage = packageRepository.findById(packageEntity.getPackageSeq()).get();
+        assertEquals(1, updatedCount);
+        assertEquals(10, findPackage.getCount());
+        assertEquals(180, findPackage.getPeriod());
+    }
+
+    @Test
+    public void test_delete(){
+        //given
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("제거할 이용권");
+        packageEntity.setPeriod(1);
+        PackageEntity newPackageEntity = packageRepository.save(packageEntity);
+
+        //when
+        packageRepository.deleteById(newPackageEntity.getPackageSeq());
+
+        //then
+        assertFalse(packageRepository.findById(packageEntity.getPackageSeq()).isPresent());
     }
 
 
