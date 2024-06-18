4c95e44202abaec8196643fc66603aab90feceb3
Kyle Kim <dkim577@wisc.edu>
2024-06-19 07:25:17 +0900

    Add BulkPassEntities

---
+++ src/main/java/com/fitness/pass/pass_batch/repository/pass/BulkPassRepository.java
@@ -0,0 +1,6 @@
+package com.fitness.pass.pass_batch.repository.pass;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BulkPassRepository extends JpaRepository<BulkPassEntity,Integer> {
+}
---
+++ src/main/java/com/fitness/pass/pass_batch/repository/pass/BulkPassStatus.java
@@ -0,0 +1,5 @@
+package com.fitness.pass.pass_batch.repository.pass;
+
+public enum BulkPassStatus {
+    READY, COMPLETED
+}
