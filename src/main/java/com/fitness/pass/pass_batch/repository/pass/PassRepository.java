523f74ec53e5ff0bfeaaee9c99dc65deb9426e72
Kyle Kim <dkim577@wisc.edu>
2024-06-29 16:53:15 +0900

    Async execution Done

--- src/main/java/com/fitness/pass/pass_batch/repository/pass/PassRepository.java
+++ src/main/java/com/fitness/pass/pass_batch/repository/pass/PassRepository.java
@@ -1,9 +1,19 @@
 package com.fitness.pass.pass_batch.repository.pass;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface PassRepository extends JpaRepository<PassEntity, Integer> {
-
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE PassEntity p" +
+            "          SET p.remainingCount = :remainingCount," +
+            "              p.modifiedAt = CURRENT_TIMESTAMP" +
+            "        WHERE p.passSeq = :passSeq")
+    int updateRemainingCount(Integer passSeq, Integer remainingCount);
 }
